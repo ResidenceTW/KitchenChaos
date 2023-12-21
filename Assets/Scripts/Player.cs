4d5fdf8dd97d9c5f4391ab076782fe42f348e500
ResidenceTW <a890305555@gmail.com>
2023-12-21 15:11:00 +0800

    玩家動畫

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,10 +35,18 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0 , inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime) ;
 
         Debug.Log (transform.forward);
         
     }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
 }
